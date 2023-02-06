039dfe06cc2be20ba4a1f21989d4443d1875d815
scottmclellan <me@scott-mclellan.com>
2023-02-06 09:43:23 -0500

    added MainDashboard component, installed the elements-plus ui library

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainDashboard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainDashboard from './components/MainDashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainDashboard
   }
 }
 </script>
---
+++ src/components/MainDashboard.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-container>
+    <el-header style="height:100px">
+      <h1>{{ currentMonth }}</h1>
+      <p>Total Money Spent: {{ totalSpent }}</p>
+    </el-header>
+    <el-main>
+      <el-tree
+        :data="categories"
+        :props="{ label: 'name', children: 'subCategories' }"
+        :render-content="renderContent"
+      />
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentMonth: "February 2023",
+      totalSpent: "$1,000",
+      categories: [
+        {
+          name: "Food",
+          amountSpent: "$500",
+          subCategories: [
+            {
+              name: "Restaurants",
+              amountSpent: "$200",
+            },
+            {
+              name: "Grocery",
+              amountSpent: "$300",
+            },
+          ],
+        },
+        {
+          name: "Transportation",
+          amountSpent: "$300",
+          subCategories: [
+            {
+              name: "Gas",
+              amountSpent: "$100",
+            },
+            {
+              name: "Public Transportation",
+              amountSpent: "$200",
+            },
+          ],
+        },
+        {
+          name: "Entertainment",
+          amountSpent: "$200",
+        },
+      ],
+    };
+  },
+  methods: {
+    renderContent(h, { node, data }) {
+      return h("span", [
+        h("span", node.label),
+        h("span", { style: { float: "right" } }, data.amountSpent),
+      ]);
+    },
+  },
+};
+</script>
